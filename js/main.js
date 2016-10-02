9cf630cd217104f019f3cd8d950b11994edc6f40
missingdays <rebovykin@gmail.com>
2016-10-02 16:43:52 +0300

    Add date utils

--- js/main.js
+++ js/main.js
@@ -28,4 +28,3 @@ function ifEnterPressed(event) {
         onSearchInputClicked();
     }
 }
-
--- js/storage.js
+++ js/storage.js
@@ -50,3 +50,48 @@ Storage.prototype.addNote = function(note){
 Storage.prototype.getAllNotes = function(){
     return JSON.parse(localStorage[this.name]);
 }
+
+
+/**
+ * Returns current timestamp
+ */
+function getTimestamp(){
+    var d = new Date();
+    return d.getTime();
+}
+
+/**
+ * Formats timestamp in human readable format
+ *
+ * Example
+ * -------
+ *
+ * var t = getTimestamp();
+ * formatTimestamp(t);
+ */
+function formatTimestamp(ms){
+    var d = new Date(ms);
+
+    var seconds = align(d.getSeconds());
+    var minutes = align(d.getMinutes());
+    var hours = align(d.getHours());
+
+    var days = align(d.getDate());
+    var month = align(d.getMonth());
+    var year = d.getFullYear();
+
+    var s = year + "-" + month + "-" + days + " " + hours + ":" + minutes + ":" + seconds;
+
+    return s;
+}
+
+/**
+ * Aligns time so it has leading zero if needed
+ */
+function align(v){
+    if(v < 10){
+        return '0' + v;
+    }
+
+    return v;
+}
